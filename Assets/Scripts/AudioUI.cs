844fc7e182e073628ce275fa752dc4a464332d4d
Gustavo Beltrao Braga <gbeltrab@emeal.nttdata.com>
2023-04-25 19:17:54 -0300

    Updates

--- Assets/Scripts/AudioUI.cs
+++ Assets/Scripts/AudioUI.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class AudioUI : MonoBehaviour
 {
-    [SerializeField] private float _volume = 1f;
+    [SerializeField] private float _volume = .5f;
     [SerializeField] private GameObject _volumeSlider;
     private Vector3 sliderScale;
 
     private void Start()
     {
         sliderScale = _volumeSlider.transform.localScale;
+        SetAudio(_volume);
     }
 
     private void OnMouseOver()
     {
         if (Input.mouseScrollDelta.y < 0f)
-        {
             _volume += Time.deltaTime;
-            _volume = Mathf.Clamp(_volume, 0.00001f, 1f);
-            _volumeSlider.transform.localScale = new Vector3(_volume * sliderScale.x, sliderScale.y, sliderScale.z);
-        }
         else if (Input.mouseScrollDelta.y > 0f)
-        {
             _volume -= Time.deltaTime;
-            _volume = Mathf.Clamp(_volume, 0.00001f, 1f);
-            _volumeSlider.transform.localScale = new Vector3(_volume * sliderScale.x, sliderScale.y, sliderScale.z);
-        }
 
-        AudioHelper.Instance.SetMasterVolume(_volume);
+        SetAudio(_volume);
+    }
+
+    private void SetAudio(float vol)
+    {
+        vol = Mathf.Clamp(vol, 0.00001f, 1f);
+        _volumeSlider.transform.localScale = new Vector3(vol * sliderScale.x, sliderScale.y, sliderScale.z);
+        AudioHelper.Instance.SetMasterVolume(vol);
     }
 
     public void Disable()
